9a2ce925b2ce33ea5a7aabbe317b0d32c9954cdc
Kaidanov <2bhere4u@gmail.com>
2017-02-18 18:22:59 +0200

    Add project files.

---
+++ Kai.MVC.Skeleton/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Kai.MVC.Skeleton.MvcApplication" Language="C#" %>
---
+++ Kai.MVC.Skeleton/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kai.MVC.Skeleton.Startup))]
+namespace Kai.MVC.Skeleton
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
